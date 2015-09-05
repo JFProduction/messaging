1f1a795fef9094551f6767267fc23b9350aa36da
jimmyfargo <zblizzard@live.com>
2015-09-05 18:59:24 -0400

    enter key working

--- public/js/common.js
+++ public/js/common.js
@@ -1,17 +1,17 @@
-// var spacePatt = /\s/g+;
 $(document).ready(function() {
-	$('.user-text').on('keyup', function(e) {
-	    if (e.which == 13) {
-	        sendMessage();
-	    }
-	});
+    $('.user-text').on('keyup', function(e) {
+        if (e.which == 13) {
+            sendMessage();
+        }
+    });
 
-	$('#user-name').on('keyup', function(e) {
-	    if (e.which == 13) {
-	        createUser();
-	    }
-	});
+    $('#user-name').on('keyup', function(e) {
+        if (e.which == 13) {
+            createUser();
+        }
+    });
 });
+
 
 function cleanInput(input) {
 	return "<div>" + input + "</div>";
